e62e0121615f8ed541973288690aaaffa8522e0a
wangbangpc <79408854@163.com>
2017-12-08 15:37:11 +0800

    change route

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+$api = app('api.router');
+$api->version('v1', ['namespace' => 'App\Http\Controllers'], function ($api) {
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    /** @var \Dingo\Api\Routing\Router $api */
+    $api->get('/', function () {
+        //測試路由
+        return  "当前服务器版本" . app()->version();
+    });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//    $api->resource('reports', 'ReportController');
+
+  //  $api->group(['middleware' => 'jwt.auth'], function ($api) {
+    //
+   // });
+
 });
